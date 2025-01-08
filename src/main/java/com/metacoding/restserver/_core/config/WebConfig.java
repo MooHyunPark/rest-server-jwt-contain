f2336fcdbdaea572482ea6c58ab2d4c12c93863f
angus <angus1208@naver.com>
2025-01-08 11:27:35 +0900

    [feat] server 프로젝트 세팅

---
+++ src/main/java/com/metacoding/restserver/_core/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.metacoding.restserver._core.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        WebMvcConfigurer.super.addResourceHandlers(registry);
+
+        registry
+                .addResourceHandler("/images/**")
+                .addResourceLocations("file:" + "./images/")
+                .setCachePeriod(60 * 60);
+
+    }
+}
